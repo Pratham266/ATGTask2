cdecef112df411b243eed77883a37983cd3f04d8
Barot Pratham Janakkumar <barotpratham30@gmail.com>
2023-06-05 19:52:47 +0530

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <script src="https://cdn.tailwindcss.com" async></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
